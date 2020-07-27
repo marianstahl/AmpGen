f199e970ddc5dc7884451cf7e9a31e8e1f817df6
mstahl <marian.stahl@cern.ch>
2020-07-27 10:01:57 -0400

    update i dont want to loose

--- examples/BaryonFitter.cpp
+++ examples/BaryonFitter.cpp
@@ -89,7 +89,6 @@ FitResult* doFit( PDF&& pdf, EventList_type& data, EventList_type& mc, MinuitPar
   auto time      = std::clock();
 
   pdf.setEvents( data );
-  //for_each( pdf.pdfs(), [](auto& f ){ f.prepare(); } );
 
   /* Minimiser is a general interface to Minuit1/Minuit2,
      that is constructed from an object that defines an operator() that returns a double
@@ -122,23 +121,23 @@ FitResult* doFit( PDF&& pdf, EventList_type& data, EventList_type& mc, MinuitPar
     counter++;
   } );
 
-  auto evaluator     = pdf.componentEvaluator(&mc);
-  auto projections   = data.eventType().defaultProjections(100);
-  projections.emplace_back( decayAngle0, "angle0", "\\theta_{z}"       ,100,0,180.   , "^{\\mathrm{o}}");
-  projections.emplace_back( decayAngle1, "angle1", "\\phi_{x}"         ,100,-180,180 , "^{\\mathrm{o}}");
-  projections.emplace_back( decayAngle2, "angle2", "\\phi_{h^{+}h^{-}}",100,0,180    , "^{\\mathrm{o}}");
-  projections.emplace_back( HelicityCosine({0},{1},{1,2}), "hCos1", "\\cos\\left(\\theta\\right)",100,-1.,1);
-  projections.emplace_back( HelicityCosine({0},{2},{0,1}), "hCos2", "\\cos\\left(\\theta^{\\prime}\\right)",100,-1.,1);
-
-  /* Write out the data plots. This also shows the first example of the named arguments
-     to functions, emulating python's behaviour in this area */
-  auto evaluator_per_component = std::get<0>( pdf.pdfs() ).componentEvaluator();
-  for( const auto& proj : projections )
-  {
-    proj(mc, evaluator,                                           PlotOptions::Norm(data.size()), PlotOptions::AutoWrite() );
-    proj(mc, evaluator_per_component, PlotOptions::Prefix("amp"), PlotOptions::Norm(data.size()), PlotOptions::AutoWrite() );
-    proj(data, PlotOptions::Prefix("Data") )->Write();
-  }
+  // auto evaluator     = pdf.componentEvaluator(&mc);
+  // auto projections   = data.eventType().defaultProjections(100);
+  // projections.emplace_back( decayAngle0, "angle0", "\\theta_{z}"       ,100,0,180.   , "^{\\mathrm{o}}");
+  // projections.emplace_back( decayAngle1, "angle1", "\\phi_{x}"         ,100,-180,180 , "^{\\mathrm{o}}");
+  // projections.emplace_back( decayAngle2, "angle2", "\\phi_{h^{+}h^{-}}",100,0,180    , "^{\\mathrm{o}}");
+  // projections.emplace_back( HelicityCosine({0},{1},{1,2}), "hCos1", "\\cos\\left(\\theta\\right)",100,-1.,1);
+  // projections.emplace_back( HelicityCosine({0},{2},{0,1}), "hCos2", "\\cos\\left(\\theta^{\\prime}\\right)",100,-1.,1);
+  //
+  // /* Write out the data plots. This also shows the first example of the named arguments
+  //    to functions, emulating python's behaviour in this area */
+  // auto evaluator_per_component = std::get<0>( pdf.pdfs() ).componentEvaluator();
+  // for( const auto& proj : projections )
+  // {
+  //   proj(mc, evaluator,                                           PlotOptions::Norm(data.size()), PlotOptions::AutoWrite() );
+  //   proj(mc, evaluator_per_component, PlotOptions::Prefix("amp"), PlotOptions::Norm(data.size()), PlotOptions::AutoWrite() );
+  //   proj(data, PlotOptions::Prefix("Data") )->Write();
+  // }
 
   return fr;
 }
@@ -160,16 +159,20 @@ int main( int argc, char* argv[] )
   OptionsParser::setArgs( argc, argv );
 
   const std::vector<std::string> dataFile = NamedParameter<std::string>("DataSample","").getVector();
-  const std::string simFile               = NamedParameter<std::string>("SgIntegratorFname", ""   , "Name of file containing simulated sample for using in MC integration");
+  const std::string simFile               = NamedParameter<std::string>("SimFile", ""   , "Name of file containing simulated sample for using in MC integration");
   const std::string logFile               = NamedParameter<std::string>("LogFile","Fitter.log");
   const std::string plotFile              = NamedParameter<std::string>("Plots","plots.root");
   const std::string prefix                = NamedParameter<std::string>("PlotPrefix","");
   const std::string idbranch              = NamedParameter<std::string>("IDBranch","");
-  const std::string weight_branch         = NamedParameter<std::string>("WeightBranch","weight","Name of branch containing event weights.");
+  const std::string mcidbranch            = NamedParameter<std::string>("MCIDBranch","");
+  const std::string weight_branch         = NamedParameter<std::string>("WeightBranch","","Name of branch containing event weights.");
+  const std::string mc_weight_branch      = NamedParameter<std::string>("MCWeightBranch","","Name of branch containing event weights.");
 
   const auto nev_MC = NamedParameter<int>("NEventsMC", 8e6, "Number of MC events for normalization.");
-  auto bNames = NamedParameter<std::string>("Branches", std::vector<std::string>()
-              ,"List of branch names, assumed to be \033[3m daughter1_px ... daughter1_E, daughter2_px ... \033[0m" ).getVector();
+  auto bNames = NamedParameter<std::string>("Branches", std::vector<std::string>(),
+                                            "List of branch names, assumed to be \033[3m daughter1_px ... daughter1_E, daughter2_px ... \033[0m" ).getVector();
+  auto MCbNames = NamedParameter<std::string>("MCBranches", std::vector<std::string>(),
+                                              "List of branch names, assumed to be \033[3m daughter1_px ... daughter1_E, daughter2_px ... \033[0m" ).getVector();
   auto pNames = NamedParameter<std::string>("EventType" , ""
               , "EventType to fit, in the format: \033[3m parent daughter1 daughter2 ... \033[0m" ).getVector();
 
@@ -203,48 +206,56 @@ int main( int argc, char* argv[] )
   /* Events are read in from ROOT files. If only the filename and the event type are specified,
      the file is assumed to be in the specific format that is defined by the event type,
      unless the branches to load are specified in the user options */
-  EventType evtType_primed = evtType;
-  if(!idbranch.empty())
-    evtType_primed.extendEventType(idbranch);
-  EventList_type events(dataFile, evtType_primed, Branches(bNames), GetGenPdf(false), WeightBranch(weight_branch) );
+  EventList_type events(dataFile, evtType, Branches(bNames), GetGenPdf(false), WeightBranch(weight_branch), ExtraBranches(std::vector<std::string>{idbranch}) );
 
   /* Generate events to normalise the PDF with. This can also be loaded from a file,
      which will be the case when efficiency variations are included. */
   EventList_type eventsMC = simFile == ""
    ? EventList_type(Generator<RecursivePhaseSpace, EventList>(sig.matrixElements()[0].decayTree.quasiStableTree(), events.eventType(), &rndm).generate(nev_MC))
-   : EventList_type(simFile, events.eventType());
+   : EventList_type(simFile, evtType, Branches(MCbNames), WeightBranch(mc_weight_branch), ExtraBranches(std::vector<std::string>{mcidbranch}));
 
   /* Transform data if we have an ID brach. That branch indicates that we operate on a sample with particles+antiparticles mixed.
      The transformation also includes boosting to the restframe of the head of the decay chain.
      TODO: There might be situations where you want to separate both transformations */
-  if(!idbranch.empty()){
-    auto frame_transform = [&evtType](auto& event){
-      TVector3 pBeam(0,0,1);
-      if( event[event.size()-1] < 0 ){
-        invertParity( event, 3);
-        pBeam = -pBeam;
-      }
-      std::vector<unsigned> daughters_as_ints(evtType.size());
-      std::iota (daughters_as_ints.begin(), daughters_as_ints.end(), 0u);
-      TLorentzVector pP = pFromEvent(event,daughters_as_ints);
-      //if( pP.P() < 10e-5) return;
-      TVector3 pZ = pP.Vect();
-      rotateBasis( event, (pBeam.Cross(pZ) ).Cross(pZ), pBeam.Cross(pZ), pZ );
-      boost( event, {0, 0, -1}, pP.P()/pP.E() );
-    };
+  auto const n_final_state_particles = evtType.size();
+  std::vector<unsigned> daughters_as_ints(n_final_state_particles);
+  std::iota (daughters_as_ints.begin(), daughters_as_ints.end(), 0u);
+  auto frame_transform = [&evtType, &daughters_as_ints, &n_final_state_particles](auto& event){
+    TVector3 pBeam(0,0,1);
+    if( event[event.size()-1] < 0 ){
+      invertParity( event, n_final_state_particles);
+      pBeam = -pBeam;
+    }
+    TLorentzVector pP = pFromEvent(event,daughters_as_ints);
+    //if( pP.P() < 10e-5) return;
+    TVector3 pZ = pP.Vect();
+    rotateBasis( event, (pBeam.Cross(pZ) ).Cross(pZ), pBeam.Cross(pZ), pZ );
+    boost( event, {0, 0, -1}, pP.P()/pP.E() );
+  };
 
-    for( auto& event : events )
-      if( event[event.size()-1] < 0 ){
-        event.print();
-        break;
-      }
-    events.transform( frame_transform );
-    for( auto& event : events )
-      if( event[event.size()-1] < 0 ){
-        event.print();
-        break;
-      }
-  }
+
+  for( auto& event : events )
+    if( event[event.size()-1] < 0 ){
+      event.print();
+      break;
+    }
+  events.transform( frame_transform );
+  for( auto& event : events )
+    if( event[event.size()-1] < 0 ){//E if there's no ID branch
+      event.print();
+      break;
+    }
+  for( auto& event : eventsMC )
+    if( event[event.size()-1] < 0 ){
+      event.print();
+      break;
+    }
+  eventsMC.transform( frame_transform );
+  for( auto& event : eventsMC )
+    if( event[event.size()-1] < 0 ){//E if there's no ID branch
+      event.print();
+      break;
+    }
   sig.setMC(eventsMC);
   sig.setEvents(events);
 
